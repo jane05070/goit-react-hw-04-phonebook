4b980b10bd62fccea7fc1b80e972ff1780bf6233
jane05070 <jane05070@gmail.com>
2023-05-22 11:47:57 +0300

    contactForm done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,42 +1,44 @@
-import React, { Component } from 'react';
+import {useState} from 'react'
 import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleChange = e => {
+
+
+ const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleSubmit = e => {
+
+  const handleSubmit = e => {
     e.preventDefault();
+  onAddContact({ name, number });
 
-    this.props.onAddContact({ ...this.state });
-
-    this.setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
-
-  render() {
-    return (
-      <form className={s.TaskEditor} onSubmit={this.handleSubmit}>
+  
+return (
+      <form className={s.TaskEditor} onSubmit={handleSubmit}>
         <label className={s.TaskEditor_label}>
           Name
           <input
             className={s.TaskEditor_input}
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
         </label>
         <label className={s.TaskEditor_label}>
@@ -45,11 +47,11 @@ export default class ContactForm extends Component {
             className={s.TaskEditor_input}
             type="text"
             name="number"
-            value={this.state.number}
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
         </label>
         <button className={s.TaskEditor_button} type="submit">
@@ -58,10 +60,73 @@ export default class ContactForm extends Component {
       </form>
     );
   }
-}
 
-ContactForm.propTypes = {
+  ContactForm.propTypes = {
   onAddContact: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
 };
+
+export default ContactForm;
+
+// export default class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onAddContact({ ...this.state });
+
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form className={s.TaskEditor} onSubmit={this.handleSubmit}>
+//         <label className={s.TaskEditor_label}>
+//           Name
+//           <input
+//             className={s.TaskEditor_input}
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label className={s.TaskEditor_label}>
+//           Number
+//           <input
+//             className={s.TaskEditor_input}
+//             type="text"
+//             name="number"
+//             value={this.state.number}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <button className={s.TaskEditor_button} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+// ContactForm.propTypes = {
+//   onAddContact: PropTypes.func.isRequired,
+//   name: PropTypes.string.isRequired,
+//   number: PropTypes.string.isRequired,
+// };
